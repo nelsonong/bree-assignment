d05facc0788dbff56d3f88b804643c0108408ad7
nelsonong <nelsonong3@gmail.com>
2022-05-08 19:53:27 -0400

    Add GET functions for getting a list of users and their transactions by email

--- server/server.js
+++ server/server.js
@@ -28,6 +28,38 @@ const {
 const app = express();
 app.use(cors());
 
+app.get('/users', async (req, res) => {
+    try {
+        const docs = await client.query(
+            Map(
+                Paginate(Documents(Collection('Users'))),
+                Lambda(x => Get(x))
+            )
+        );
+        const users = docs.data.map(doc => doc.data);
+        res.send(users);
+    } catch (e) {
+        res.send(e);
+    }
+});
+
+app.get('/predictions/:email', async (req, res) => {
+    try {
+        const docs = await client.query(
+            Map(
+                Paginate(Match(Index('transactions_by_email'), req.params.email)),
+                Lambda(x => Get(x))
+            )
+        );
+        if (docs.data.length) {
+            const { transactions } = docs.data[0].data;
+            res.send(transactions);
+        }
+    } catch (e) {
+        res.send(e);
+    }
+});
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
